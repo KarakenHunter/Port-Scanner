c13a8e6fecd05fe138eb55612bc18d8fb5c71886
KarakenHunter <sharmamanthan474@gmail.com>
2026-03-20 23:39:16 +0530

    feat:log scan results to file

--- scanner.py
+++ scanner.py
@@ -1,16 +1,18 @@
 import socket
 import errno
 
-for port in range(1, 65536):
-    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result=sock.connect_ex(('localhost', port))
-    if result == 0:
-        print(f"[+] Port {port} OPEN")
-    elif result == errno.ECONNREFUSED:
-        print(f"[-] Port {port} is CLOSED")
-    elif result == errno.ETIMEDOUT:
-        print(f"[?] Port {port} FILTERED")
-    else:
-        print(f"[!] Port {port} UNKNOWN ({result})")
-    sock.close()
+with open("Logs.txt", 'a') as f:
+    for port in range(1, 65536):
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result=sock.connect_ex(('localhost', port))
+        if result == 0:
+            f.write(f"[+] Port {port} OPEN\n")
+        elif result == errno.ECONNREFUSED:
+            pass
+        elif result == errno.ETIMEDOUT:
+            f.write(f"[?] Port {port} FILTERED")
+        else:
+            f.write(f"[!] Port {port} UNKNOWN ({result})")
+        sock.close()
+print("Data Successfully saved in Logs.txt!")
